87c3cf3381770a96a8e8eff8a31a4e86c86942b4
MariiaHula <mariyagulaya@gmail.com>
2023-12-10 14:14:21 +0200

    Update CarPreviewList.jsx
    fix problem loader

--- src/components/CarPreviewList.jsx
+++ src/components/CarPreviewList.jsx
@@ -80,148 +80,132 @@ const CarPreviewList = () => {
 
   return (
     <>
-      {isLoading ? (
+      <section>
         <div className="flex flex-col items-center w-full px-[120px] max-w-[1440px]">
-          <MutatingDots
-            height="100"
-            width="100"
-            color="#3470ff"
-            secondaryColor="#3470ff"
-            radius="12.5"
-            ariaLabel="mutating-dots-loading"
-            wrapperStyle={{}}
-            wrapperClass=""
-            visible={true}
-          />
-        </div>
-      ) : (
-        <section>
-          <div className="flex flex-col items-center w-full px-[120px] max-w-[1440px]">
-            {allCarsData.length === 0 ? (
-              <p className=" text-[#3470ff] w-fit mt-[-1.00px] [font-family:'Manrope-Medium',Helvetica] font-medium text-transparent text-[28px] tracking-[0] leading-[24px] whitespace-nowrap">
-                Sorry, no matches
-              </p>
-            ) : (
-              <>
-                <ul className="grid grid-cols-4 gap-6 mb-10">
-                  {(carBrand || priceOneOur || from || to
-                    ? allCarsData
-                    : displayCars
-                  )?.map(car => (
-                    <li
-                      className="bg-[#ffffff] flex flex-col justify-center w-[274px] rounded-[14px] relative"
-                      key={car.id}
+          {allCarsData.length === 0 ? (
+            <p className=" text-[#3470ff] w-fit mt-[-1.00px] [font-family:'Manrope-Medium',Helvetica] font-medium text-transparent text-[28px] tracking-[0] leading-[24px] whitespace-nowrap">
+              Sorry, no matches
+            </p>
+          ) : (
+            <>
+              <ul className="grid grid-cols-4 gap-6 mb-10">
+                {(carBrand || priceOneOur || from || to
+                  ? allCarsData
+                  : displayCars
+                )?.map(car => (
+                  <li
+                    className="bg-[#ffffff] flex flex-col justify-center w-[274px] rounded-[14px] relative"
+                    key={car.id}
+                  >
+                    <button
+                      onClick={() => handleFavoriteClick(car.id)}
+                      className="absolute top-[14px] right-[14px] w-[20px] h-[20px]"
                     >
-                      <button
-                        onClick={() => handleFavoriteClick(car.id)}
-                        className="absolute top-[14px] right-[14px] w-[20px] h-[20px]"
-                      >
-                        {favorites.includes(car.id) ? (
-                          <BlueHeart />
-                        ) : (
-                          <WhiteHeart />
-                        )}
-                      </button>
-                      <div className="flex items-center justify-center max-w-[274px] h-[274px] rounded-[14px] overflow-hidden mb-[14px] mr-0 ml-0">
-                        <img
-                          className="w-full h-full object-cover m-0"
-                          src={
-                            car.img
-                              ? `${car.img}`
-                              : 'https://easydrawingguides.com/wp-content/uploads/2017/02/how-to-draw-a-cartoon-car-featured-image-1200.png'
-                          }
-                          alt={car.make}
-                        />
+                      {favorites.includes(car.id) ? (
+                        <BlueHeart />
+                      ) : (
+                        <WhiteHeart />
+                      )}
+                    </button>
+                    <div className="flex items-center justify-center max-w-[274px] h-[274px] rounded-[14px] overflow-hidden mb-[14px] mr-0 ml-0">
+                      <img
+                        className="w-full h-full object-cover m-0"
+                        src={
+                          car.img
+                            ? `${car.img}`
+                            : 'https://easydrawingguides.com/wp-content/uploads/2017/02/how-to-draw-a-cartoon-car-featured-image-1200.png'
+                        }
+                        alt={car.make}
+                      />
+                    </div>
+                    <div className="flex flex-col max-w-[274px] items-center">
+                      <div className="flex items-between justify-between w-full  mb-[8px]">
+                        <p className="w-fit mt-[-1.00px] [font-family:'Manrope-Medium',Helvetica] font-medium text-transparent text-[18px] tracking-[0] leading-[24px] whitespace-nowrap">
+                          <span className="text-[#121417]"> {car.make} </span>
+                          {/* <span className="text-[#3470ff]">{car.model}</span> */}
+                          <span className="text-[#121417]">, {car.year}</span>
+                        </p>
+                        <p className=" w-fit mt-[-1.00px] [font-family:'Manrope-Medium',Helvetica] font-medium text-transparent text-[18px] tracking-[0] leading-[24px] whitespace-nowrap]">
+                          <span className="text-[#121417] mr-[14px]">
+                            {car.rentalPrice}
+                          </span>
+                        </p>
                       </div>
-                      <div className="flex flex-col max-w-[274px] items-center">
-                        <div className="flex items-between justify-between w-full  mb-[8px]">
-                          <p className="w-fit mt-[-1.00px] [font-family:'Manrope-Medium',Helvetica] font-medium text-transparent text-[18px] tracking-[0] leading-[24px] whitespace-nowrap">
-                            <span className="text-[#121417]"> {car.make} </span>
-                            {/* <span className="text-[#3470ff]">{car.model}</span> */}
-                            <span className="text-[#121417]">, {car.year}</span>
-                          </p>
-                          <p className=" w-fit mt-[-1.00px] [font-family:'Manrope-Medium',Helvetica] font-medium text-transparent text-[18px] tracking-[0] leading-[24px] whitespace-nowrap]">
-                            <span className="text-[#121417] mr-[14px]">
-                              {car.rentalPrice}
-                            </span>
-                          </p>
-                        </div>
 
-                        <div className="flex flex-col max-w-[274px] items-start gap-[4px]  mb-[28px]">
-                          <div className="inline-flex items-start  gap-[6px] flex-wrap flex-[0_0_auto] overflow-hidden">
-                            <p className="w-fit mt-[-1.00px] [font-family:'Manrope-Regular',Helvetica] font-normal text-[#12141780] text-[12px] tracking-[0] leading-[18px] whitespace-nowrap">
-                              {car.address.split(',')[1].trim()}
-                            </p>
-                            <Line />
-                            <p className=" mt-[-1.00px] [font-family:'Manrope-Regular',Helvetica] font-normal text-[#12141780] text-[12px] tracking-[0] leading-[18px] whitespace-nowrap">
-                              Ukraine
-                            </p>
-                            <Line />
-                            <p className=" mt-[-1.00px] [font-family:'Manrope-Regular',Helvetica] font-normal text-[#12141780] text-[12px] tracking-[0] leading-[18px] whitespace-nowrap">
-                              {car.rentalCompany}
-                            </p>
-                            <Line />
-                            <p className="mt-[-1.00px] [font-family:'Manrope-Regular',Helvetica] text-[#12141780] text-[12px] tracking-[0] leading-[18px] whitespace-nowrap">
-                              {car.type}
-                            </p>
-                            {/* </div> */}
-                            {/* <div className="inline-flex items-start  gap-[6px]  flex-[0_0_auto] overflow-hidden"> */}
-                            <p className="mt-[-1.00px] [font-family:'Manrope-Regular',Helvetica]  text-[#12141780] text-[12px] tracking-[0] leading-[18px] whitespace-nowrap">
-                              {car.make}
-                            </p>
-                            <Line />
-                            {/* <div className="inline-flex items-start gap-[6px] flex-[0_0_auto] overflow-hidden"> */}
-                            <p className="break-words w-fit mt-[-1.00px] [font-family:'Manrope-Regular',Helvetica] text-[#12141780] text-[12px] tracking-[0] leading-[18px] whitespace-nowrap">
-                              {car.id}
-                            </p>
-                            <Line />
-                            <p className="break-words w-fit mt-[-1.00px] [font-family:'Manrope-Regular',Helvetica] text-[#12141780] text-[12px] tracking-[0] leading-[18px] whitespace-nowrap">
-                              {car.accessories[0]
-                                .split(' ')
-                                .slice(0, 1)
-                                .join(' ')}
-                            </p>
-                            {/* </div> */}
-                          </div>
+                      <div className="flex flex-col max-w-[274px] items-start gap-[4px]  mb-[28px]">
+                        <div className="inline-flex items-start  gap-[6px] flex-wrap flex-[0_0_auto] overflow-hidden">
+                          <p className="w-fit mt-[-1.00px] [font-family:'Manrope-Regular',Helvetica] font-normal text-[#12141780] text-[12px] tracking-[0] leading-[18px] whitespace-nowrap">
+                            {car.address.split(',')[1].trim()}
+                          </p>
+                          <Line />
+                          <p className=" mt-[-1.00px] [font-family:'Manrope-Regular',Helvetica] font-normal text-[#12141780] text-[12px] tracking-[0] leading-[18px] whitespace-nowrap">
+                            Ukraine
+                          </p>
+                          <Line />
+                          <p className=" mt-[-1.00px] [font-family:'Manrope-Regular',Helvetica] font-normal text-[#12141780] text-[12px] tracking-[0] leading-[18px] whitespace-nowrap">
+                            {car.rentalCompany}
+                          </p>
+                          <Line />
+                          <p className="mt-[-1.00px] [font-family:'Manrope-Regular',Helvetica] text-[#12141780] text-[12px] tracking-[0] leading-[18px] whitespace-nowrap">
+                            {car.type}
+                          </p>
+                          {/* </div> */}
+                          {/* <div className="inline-flex items-start  gap-[6px]  flex-[0_0_auto] overflow-hidden"> */}
+                          <p className="mt-[-1.00px] [font-family:'Manrope-Regular',Helvetica]  text-[#12141780] text-[12px] tracking-[0] leading-[18px] whitespace-nowrap">
+                            {car.make}
+                          </p>
+                          <Line />
+                          {/* <div className="inline-flex items-start gap-[6px] flex-[0_0_auto] overflow-hidden"> */}
+                          <p className="break-words w-fit mt-[-1.00px] [font-family:'Manrope-Regular',Helvetica] text-[#12141780] text-[12px] tracking-[0] leading-[18px] whitespace-nowrap">
+                            {car.id}
+                          </p>
+                          <Line />
+                          <p className="break-words w-fit mt-[-1.00px] [font-family:'Manrope-Regular',Helvetica] text-[#12141780] text-[12px] tracking-[0] leading-[18px] whitespace-nowrap">
+                            {car.accessories[0]
+                              .split(' ')
+                              .slice(0, 1)
+                              .join(' ')}
+                          </p>
+                          {/* </div> */}
                         </div>
-                        <button
-                          type="button"
-                          onClick={() => handleChooseCar(car.id)}
-                          className="gap-0 flex-row justify-center items-center h-[40px] bg-[#3470ff] hover:bg-[#0B44CD] focus:bg-[#0B44CD] transition duration-300 ease-in-out px-11 py-3.5 rounded-xl flex w-full"
-                        >
-                          <div className="w-fit mt-[-1,00px] text-white text-[14px] tracking-[0] leading-5 [font-family:'Manrope-SemiBold', Helvetica] whitespace-nowrap">
-                            Learn More
-                          </div>
-                        </button>
                       </div>
-                    </li>
-                  ))}
-                </ul>
-              </>
+                      <button
+                        type="button"
+                        onClick={() => handleChooseCar(car.id)}
+                        className="gap-0 flex-row justify-center items-center h-[40px] bg-[#3470ff] hover:bg-[#0B44CD] focus:bg-[#0B44CD] transition duration-300 ease-in-out px-11 py-3.5 rounded-xl flex w-full"
+                      >
+                        <div className="w-fit mt-[-1,00px] text-white text-[14px] tracking-[0] leading-5 [font-family:'Manrope-SemiBold', Helvetica] whitespace-nowrap">
+                          Learn More
+                        </div>
+                      </button>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
+          {displayCars.length < 32 &&
+            !carBrand &&
+            !priceOneOur &&
+            !from &&
+            !to && (
+              <button
+                onClick={handleLoadMoreCkick}
+                disabled={isLoading}
+                className={`mb-[100px] text-[16px] [font-family:'Montserrat-Medium',Helvetica] text-[#3470ff] hover:text-[#0B44CD] focus:text-[#0B44CD] transition duration-300 ease-in-out  ${
+                  location.pathname !== '/catalog' ? 'hidden' : null
+                }`}
+              >
+                {isLoading ? 'Loading...' : 'Load More'}
+              </button>
             )}
-            {displayCars.length < 32 &&
-              !carBrand &&
-              !priceOneOur &&
-              !from &&
-              !to && (
-                <button
-                  onClick={handleLoadMoreCkick}
-                  disabled={isLoading}
-                  className={`mb-[100px] text-[16px] [font-family:'Montserrat-Medium',Helvetica] text-[#3470ff] hover:text-[#0B44CD] focus:text-[#0B44CD] transition duration-300 ease-in-out  ${
-                    location.pathname !== '/catalog' ? 'hidden' : null
-                  }`}
-                >
-                  {isLoading ? 'Loading...' : 'Load More'}
-                </button>
-              )}
-            {isOpen && (
-              <Modal closeModal={closeModal}>
-                <CarCard closeModal={closeModal} carId={carDetails} />
-              </Modal>
-            )}
-          </div>
-        </section>
-      )}
+          {isOpen && (
+            <Modal closeModal={closeModal}>
+              <CarCard closeModal={closeModal} carId={carDetails} />
+            </Modal>
+          )}
+        </div>
+      </section>
     </>
   );
 };
